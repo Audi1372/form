36ab4be1a9fa0de875f8d633a104040f3b56b478
Audi1372 <115685680+Audi1372@users.noreply.github.com>
2022-11-16 15:58:30 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,29 +1,51 @@
 
-
 const mybtn = document.querySelector(".btn")
 mybtn.addEventListener("click", ()=>{
   var row;
   var ele;
+  var gender
 var ele = document.getElementsByName('gender');
       
     for(i = 0; i < ele.length; i++) {
         if(ele[i].checked){
-        document.getElementById("demo1").innerHTML
-              = (ele[i].value);
+        gender  = ele[i].value;
+         
     }
   }
+
+  var checkboxes=document.querySelectorAll('.checkbox')
+  var food=[]
+  for(j = 0; j < checkboxes.length; j++){
   
+  if(checkboxes[j].checked){
+    food.push(checkboxes[j].value)
+  }
+}
+  
+
+    
+    const formData= {};
+    formData["firstname"]=     document.getElementById("first-name").value;
+    formData["lastname"] =     document.getElementById("last-name").value;
+    formData["address"]  =     document.getElementById("address").value;
+    formData["pincode"]  =     document.getElementById("pincode").value;
+    formData["gender"]   =     gender
+    formData["food"]     =     food;
+    formData["state"]    =     document.getElementById("state").value;
+    formData["country"]  =     document.getElementById("country").value;
+    
+
   
   
      row=`<tr>
-                    <td>${document.getElementById("first-name").value}</td>
-                    <td>${document.getElementById("last-name").value}</td>
-                    <td>${document.getElementById("address").value}</td>
-                    <td>${document.getElementById("pincode").value}</td>
-                    <td>${document.getElementById("demo1").text}</td>
-                    <td>${document.getElementById("state").value}</td>
-                    <td>${document.getElementById("country").value}</td>
-                    <td>${document.getElementById("mySelect").options.item(document.getElementById("mySelect").selectedIndex).text}</td>
+                    <td>${formData["firstname"]}</td>
+                    <td>${formData["lastname"]}</td>
+                    <td>${formData["address"]}</td>
+                    <td>${formData["pincode"]}</td>
+                    <td>${formData["gender"]}</td>
+                    <td>${formData["state"]}</td>
+                    <td>${formData["country"]}</td>
+                    <td>${formData["food"]}</td>
                   
                 </tr>`
     const tr=document.createElement("tr")
@@ -34,14 +56,13 @@ var ele = document.getElementsByName('gender');
 )
 function clear(){
   document.getElementById("first-name").value='';
-  document.getElementById("lastname").value=''
-  document.getElementById("demo").value=''
+  document.getElementById("last-name").value=''
+  
   document.getElementById("pincode").value=''
   document.getElementById("address").value=''
   document.getElementById("state").value=''
   document.getElementById("country").value=''
+ 
 }
 
   
-
- 
